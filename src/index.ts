dfc6ba9a0331284eaeb23ec72ec5f266738ad795
invalid w <wangjuesix@gmail.com>
2024-11-23 10:51:07 +0800

    feat: 完善插件更新

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,42 @@
 import pathLib from 'path';
 import fs from 'fs';
 import axios from 'axios';
-import { app, dialog } from 'electron';
 
 export class ElectronAppUpdater {
   checkVersionUrl: string;
   configJSONFileName: string;
   buildJSFileName: string;
   buildPath: string;
+  app: any;
+  dialog: any;
   constructor({
     checkVersionUrl,
     configJSONFileName,
     buildJSFileName,
+    app,
+    dialog,
   }: {
     checkVersionUrl: string;
     buildJSFileName?: string;
     configJSONFileName?: string;
+    app: any;
+    dialog: any;
   }) {
     this.checkVersionUrl = checkVersionUrl;
     this.configJSONFileName = configJSONFileName || 'config.json';
     this.buildJSFileName = buildJSFileName || 'build.js';
-    this.buildPath = pathLib.join(__dirname, '/');
+    this.buildPath = './';
+    this.app = app;
+    this.dialog = dialog;
   }
 
   async checkVersion() {
     const currentVersion = await this.getNowVersion();
     const response: any = await axios.get(this.checkVersionUrl);
     const data = response.data.result;
-    console.log(currentVersion, data);
-    const nowVersion = Number(currentVersion);
-    const remoteVersion = Number(data.version);
+    const nowVersion = currentVersion;
+    const remoteVersion = data.version;
+    console.log(currentVersion, remoteVersion);
     if (remoteVersion != nowVersion) {
       console.log('need update');
       await this.changeVersion(remoteVersion, data.url);
@@ -40,23 +47,21 @@ export class ElectronAppUpdater {
   }
 
   async changeVersion(remoteVersion: number, url: string) {
-    if (app.isPackaged) {
-      dialog.showMessageBox({
-        type: 'info',
-        title: '软件升级',
-        message: `发现新版本，v${remoteVersion}, 正在下载`,
-        buttons: ['是的'],
-      });
-      const configPath = this.getConfigPath();
-      fs.writeFileSync(configPath, JSON.stringify({ version: remoteVersion }));
-      const writePath = pathLib.join(this.buildPath, 'build.js');
-      const buildResponse: any = await axios.get(url);
-      fs.writeFileSync(writePath, buildResponse.data);
-      setTimeout(() => {
-        app.relaunch(); // 重启
-        app.exit(0);
-      }, 1200);
-    }
+    this.dialog.showMessageBox({
+      type: 'info',
+      title: '软件升级',
+      message: `发现新版本，v${remoteVersion}, 正在下载`,
+      buttons: ['是的'],
+    });
+    const configPath = this.getConfigPath();
+    fs.writeFileSync(configPath, JSON.stringify({ version: remoteVersion }));
+    const writePath = pathLib.join(this.buildPath, this.buildJSFileName);
+    const buildResponse: any = await axios.get(url);
+    fs.writeFileSync(writePath, buildResponse.data);
+    setTimeout(() => {
+      this.app.relaunch(); // 重启
+      this.app.exit(0);
+    }, 1200);
   }
 
   async getNowVersion() {
